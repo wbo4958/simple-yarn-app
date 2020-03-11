0128c98044caa87d373ba4c09361615d4ab7e96a
Eugene Chung <eugene.chung@navercorp.com>
2020-03-11 18:18:37 +0900

    adjust Client.java to Hadoop 3.1.2

--- README.md
+++ README.md
@@ -3,20 +3,22 @@ simple-yarn-app
 
 Simple YARN application to run n copies of a unix command - deliberately kept simple (with minimal error handling etc.)
 
+Build:
+======
+$ mvn clean package
+
 Usage:
 ======
 
+$ cd target
 $ export SIMPLE_APP_JAR=simple-yarn-app-1.1.0.jar
 
 ### Unmanaged mode
-
 $ hadoop jar $HADOOP_YARN_HOME/share/hadoop/yarn/hadoop-yarn-applications-unmanaged-am-launcher-2.1.1-SNAPSHOT.jar Client -classpath $SIMPLE_APP_JAR -cmd "java com.hortonworks.simpleyarnapp.ApplicationMaster /bin/date 2"
 
 ### Managed mode
 $ hadoop fs -mkdir -p /apps/simple/
-
 $ hadoop fs -copyFromLocal $SIMPLE_APP_JAR /apps/simple/$SIMPLE_APP_JAR
-
 $ hadoop jar $SIMPLE_APP_JAR com.hortonworks.simpleyarnapp.Client /bin/date 2 /apps/simple/$SIMPLE_APP_JAR
   
     
--- src/main/java/com/hortonworks/simpleyarnapp/Client.java
+++ src/main/java/com/hortonworks/simpleyarnapp/Client.java
@@ -1,45 +1,33 @@
 package com.hortonworks.simpleyarnapp;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.yarn.api.ApplicationConstants;
 import org.apache.hadoop.yarn.api.ApplicationConstants.Environment;
-import org.apache.hadoop.yarn.api.records.ApplicationId;
-import org.apache.hadoop.yarn.api.records.ApplicationReport;
-import org.apache.hadoop.yarn.api.records.ApplicationSubmissionContext;
-import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
-import org.apache.hadoop.yarn.api.records.LocalResource;
-import org.apache.hadoop.yarn.api.records.LocalResourceType;
-import org.apache.hadoop.yarn.api.records.LocalResourceVisibility;
-import org.apache.hadoop.yarn.api.records.Resource;
-import org.apache.hadoop.yarn.api.records.YarnApplicationState;
+import org.apache.hadoop.yarn.api.records.*;
 import org.apache.hadoop.yarn.client.api.YarnClient;
 import org.apache.hadoop.yarn.client.api.YarnClientApplication;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.util.Apps;
-import org.apache.hadoop.yarn.util.ConverterUtils;
 import org.apache.hadoop.yarn.util.Records;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 
 public class Client {
 
-  Configuration conf = new YarnConfiguration();
+  private final Configuration conf = new YarnConfiguration();
   
   public void run(String[] args) throws Exception {
     final String command = args[0];
-    final int n = Integer.valueOf(args[1]);
+    final int n = Integer.parseInt(args[1]);
     final Path jarPath = new Path(args[2]);
 
     // Create yarnClient
-    YarnConfiguration conf = new YarnConfiguration();
     YarnClient yarnClient = YarnClient.createYarnClient();
     yarnClient.init(conf);
     yarnClient.start();
@@ -48,25 +36,21 @@ public class Client {
     YarnClientApplication app = yarnClient.createApplication();
 
     // Set up the container launch context for the application master
-    ContainerLaunchContext amContainer = 
-        Records.newRecord(ContainerLaunchContext.class);
-    amContainer.setCommands(
-        Collections.singletonList(
-            "$JAVA_HOME/bin/java" +
-            " -Xmx256M" +
-            " com.hortonworks.simpleyarnapp.ApplicationMaster" +
-            " " + command +
-            " " + String.valueOf(n) +
-            " 1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout" + 
-            " 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr" 
-            )
-        );
+    ContainerLaunchContext amContainer = Records.newRecord(ContainerLaunchContext.class);
+    List<String> list = new ArrayList<>();
+    list.add("$JAVA_HOME/bin/java");
+    list.add("-Xmx256M");
+    list.add("com.hortonworks.simpleyarnapp.ApplicationMaster");
+    list.add(command);
+    list.add(String.valueOf(n));
+    list.add("1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout");
+    list.add("2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr");
+    amContainer.setCommands(list);
     
     // Setup jar for ApplicationMaster
     LocalResource appMasterJar = Records.newRecord(LocalResource.class);
     setupAppMasterJar(jarPath, appMasterJar);
-    amContainer.setLocalResources(
-        Collections.singletonMap("simpleapp.jar", appMasterJar));
+    amContainer.setLocalResources(Collections.singletonMap("simple-app.jar", appMasterJar));
 
     // Setup CLASSPATH for ApplicationMaster
     Map<String, String> appMasterEnv = new HashMap<String, String>();
@@ -75,12 +59,11 @@ public class Client {
     
     // Set up resource type requirements for ApplicationMaster
     Resource capability = Records.newRecord(Resource.class);
-    capability.setMemory(256);
+    capability.setMemorySize(256); //in MB
     capability.setVirtualCores(1);
 
     // Finally, set-up ApplicationSubmissionContext for the application
-    ApplicationSubmissionContext appContext = 
-    app.getApplicationSubmissionContext();
+    ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
     appContext.setApplicationName("simple-yarn-app"); // application name
     appContext.setAMContainerSpec(amContainer);
     appContext.setResource(capability);
@@ -110,7 +93,7 @@ public class Client {
   
   private void setupAppMasterJar(Path jarPath, LocalResource appMasterJar) throws IOException {
     FileStatus jarStat = FileSystem.get(conf).getFileStatus(jarPath);
-    appMasterJar.setResource(ConverterUtils.getYarnUrlFromPath(jarPath));
+    appMasterJar.setResource(URL.fromPath(jarPath));
     appMasterJar.setSize(jarStat.getLen());
     appMasterJar.setTimestamp(jarStat.getModificationTime());
     appMasterJar.setType(LocalResourceType.FILE);
@@ -118,15 +101,10 @@ public class Client {
   }
   
   private void setupAppMasterEnv(Map<String, String> appMasterEnv) {
-    for (String c : conf.getStrings(
-        YarnConfiguration.YARN_APPLICATION_CLASSPATH,
-        YarnConfiguration.DEFAULT_YARN_APPLICATION_CLASSPATH)) {
-      Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(),
-          c.trim());
+    for (String c : conf.getStrings(YarnConfiguration.YARN_APPLICATION_CLASSPATH, YarnConfiguration.DEFAULT_YARN_APPLICATION_CLASSPATH)) {
+      Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(), c.trim(), File.pathSeparator);
     }
-    Apps.addToEnvironment(appMasterEnv,
-        Environment.CLASSPATH.name(),
-        Environment.PWD.$() + File.separator + "*");
+    Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(), Environment.PWD.$() + File.separator + "*", File.pathSeparator);
   }
   
   public static void main(String[] args) throws Exception {
